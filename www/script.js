54c4dad0908ba6e888a4d24602e3158ef7874d1b
Kevin Peng <kpengboy@ocf.berkeley.edu>
2019-01-07 01:18:26 -0800

    Further js style fixes

--- www/script.js
+++ www/script.js
@@ -1,36 +1,37 @@
 'use strict';
 
-const mapping = {};
-mapping["sinkhole"] = "c1";
-mapping["destruction"] = "c2";
-mapping["firewhirl"] = "c3";
-mapping["outbreak"] = "c4";
-mapping["cyclone"] = "c5";
-mapping["venom"] = "c6";
-mapping["drought"] = "c7";
-mapping["volcano"] = "c8";
-mapping["blight"] = "c9";
-mapping["hailstorm"] = "c10";
-mapping["arsenic"] = "c11";
-mapping["heatwave"] = "c12";
-mapping["cyanide"] = "c13";
-mapping["acid"] = "c14";
-mapping["invasion"] = "c15";
-mapping["eruption"] = "c16";
-mapping["headcrash"] = "c17";
-mapping["asteroid"] = "c18";
-mapping["avalanche"] = "c19";
-mapping["blackout"] = "c20";
-mapping["plague"] = "c21";
-mapping["bigbang"] = "c22";
-mapping["wildfire"] = "c23";
-mapping["famine"] = "c24";
-mapping["madcow"] = "c25";
-mapping["surge"] = "c26";
-mapping["meteorstorm"] = "c27";
-mapping["chaos"] = "c28";
-mapping["blizzard"] = "c29";
-mapping["hurricane"] = "c32";
+const mapping = {
+    'sinkhole': 'c1',
+    'destruction': 'c2',
+    'firewhirl': 'c3',
+    'outbreak': 'c4',
+    'cyclone': 'c5',
+    'venom': 'c6',
+    'drought': 'c7',
+    'volcano': 'c8',
+    'blight': 'c9',
+    'hailstorm': 'c10',
+    'arsenic': 'c11',
+    'heatwave': 'c12',
+    'cyanide': 'c13',
+    'acid': 'c14',
+    'invasion': 'c15',
+    'eruption': 'c16',
+    'headcrash': 'c17',
+    'asteroid': 'c18',
+    'avalanche': 'c19',
+    'blackout': 'c20',
+    'plague': 'c21',
+    'bigbang': 'c22',
+    'wildfire': 'c23',
+    'famine': 'c24',
+    'madcow': 'c25',
+    'surge': 'c26',
+    'meteorstorm': 'c27',
+    'chaos': 'c28',
+    'blizzard': 'c29',
+    'hurricane': 'c32',
+};
 
 /**
  * Convert a time like "12:30:00" to a shortened time like "12:30 pm"
@@ -90,7 +91,7 @@ function get(url) {
     };
 
     req.onerror = function() {
-      reject(Error("Network Error"));
+      reject(Error('Network Error'));
     };
 
     req.send();
@@ -98,15 +99,15 @@ function get(url) {
 }
 
 function getDesktopsInUse() {
-    return get('https://www.ocf.berkeley.edu/api/lab/desktops').then(function(response) {
-        return JSON.parse(response)['public_desktops_in_use'];
-    });
+    return get('https://www.ocf.berkeley.edu/api/lab/desktops').then(
+        response => JSON.parse(response)['public_desktops_in_use']
+    );
 }
 
 function getHoursToday() {
-    return get('https://www.ocf.berkeley.edu/api/hours/today').then(function(response) {
-        return JSON.parse(response);
-    });
+    return get('https://www.ocf.berkeley.edu/api/hours/today').then(
+        response => JSON.parse(response)
+    );
 }
 
 function enableNightMode() {
@@ -126,24 +127,24 @@ function updateMap(desktopsInUse) {
 
     for (const polygon of document.getElementsByClassName('comp')) {
         if (hasOverlap(polygon.classList, idInUse)) {
-            polygon.classList.add("occupied");
-            polygon.classList.remove("available");
-            polygon.parentNode.parentNode.classList.remove("bounce");
+            polygon.classList.add('occupied');
+            polygon.classList.remove('available');
+            polygon.parentNode.parentNode.classList.remove('bounce');
         } else {
-            polygon.classList.remove("occupied");
-            polygon.classList.add("available");
-            polygon.parentNode.parentNode.classList.add("bounce");
+            polygon.classList.remove('occupied');
+            polygon.classList.add('available');
+            polygon.parentNode.parentNode.classList.add('bounce');
         }
     }
 }
 
 // Start the HTTP request to get today's hours before the page finishes loading
-var promiseGetHours = getHoursToday();
+const promiseGetHours = getHoursToday();
 
 window.onload = function() {
     // Add tooltips for mousing over the desktops
     for (const key of Object.keys(mapping)) {
-        var tooltip = document.createElement('div');
+        const tooltip = document.createElement('div');
         tooltip.classList.add('mdl-tooltip');
         tooltip.setAttribute('data-mdl-for', mapping[key]);
         tooltip.textContent = key;
@@ -151,28 +152,28 @@ window.onload = function() {
     }
 
     // Reload the page at 6am
-    var now = new Date();
-    var reloadTimer = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 6, 0, 0, 0) - now;
+    const now = new Date();
+    const reloadTimer = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 6, 0, 0, 0) - now;
     if (reloadTimer < 0) {
         reloadTimer += 24 * 60 * 60 * 1000;
     }
     setTimeout(location.reload, reloadTimer);
 
     // Update the desktops every 2.5 seconds
-    setInterval(function() { getDesktopsInUse().then(updateMap); }, 2500);
+    setInterval(() => getDesktopsInUse().then(updateMap), 2500);
 
     // Only update hours with the results from this request after the page has finished loading
-    promiseGetHours.then(function(hours) {
+    promiseGetHours.then(hours => {
         if (hours[0] !== null) {
-            hours = hours[0]
-            var start = document.getElementById('starthours');
-            var end = document.getElementById('endhours');
+            hours = hours[0];
+            const start = document.getElementById('starthours');
+            const end = document.getElementById('endhours');
 
             start.textContent = shortTime(hours[0]);
             end.textContent = shortTime(hours[1]);
 
-            closingTime = hours[1];
-            var nightTimer = new Date(now.getFullYear(), now.getMonth(), now.getDate(), closingTime, 0, 0, 0) - now;
+            const closingTime = hours[1];
+            const nightTimer = new Date(now.getFullYear(), now.getMonth(), now.getDate(), closingTime, 0, 0, 0) - now;
             if (now.getHours() >= closingTime || now.getHours() <= 5) {
                 enableNightMode();
             }
