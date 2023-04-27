93c036d684314ed5eea975e90041c67384b1ba48
Oliver Ni <oliver.ni@gmail.com>
2023-04-27 00:43:29 -0700

    feat: use transpire

---
+++ .transpire.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from transpire.resources import Deployment, Ingress, Service
+from transpire.types import Image
+from transpire.utils import get_image_tag
+
+name = "labmap"
+
+
+def objects():
+    dep = Deployment(
+        name="labmap",
+        image=get_image_tag("labmap"),
+        ports=[3000],
+    )
+
+    svc = Service(
+        name="labmap",
+        selector=dep.get_selector(),
+        port_on_pod=3000,
+        port_on_svc=80,
+    )
+
+    ing = Ingress.from_svc(
+        svc=svc,
+        host="labmap.ocf.berkeley.edu",
+        path_prefix="/",
+    )
+
+    yield dep.build()
+    yield svc.build()
+    yield ing.build()
+
+
+def images():
+    yield Image(name="labmap", path=Path("/"))
